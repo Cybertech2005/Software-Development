96890008612c41f339a176a55b8269b88b569e34
Cybertech2005 <Jlassit145@gmail.com>
2025-01-31 15:39:08 -0500

    Add files via upload

---
+++ webdevelopment/app.js
@@ -0,0 +1,11 @@
+function MyFunction() {
+    alert('THE NBA SEASON IS ALMOST HERE')
+}
+
+
+function Schedule() {
+
+alert('Draymond Green has made more threes in a game than Kevin Durant ')
+
+
+}
